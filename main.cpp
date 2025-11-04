72bc854199a29f5bb0ad44922b93170bc35a0d4d
mfunchion <mfunch02@villanova.edu>
2025-11-04 15:16:16 -0500

    Created the c++ file to work on

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Lab 5
+//By: Matthew Funchion, Alex Wilkinson, and Ian Murray
+//Testing the times of sorting algorithms
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World" << endl;
+	return 0;
+}
+
+
+

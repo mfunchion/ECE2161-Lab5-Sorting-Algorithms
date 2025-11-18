d25eb13e73a80430e3260dbece0552d8a79af14f
mfunchion <mfunch02@villanova.edu>
2025-11-18 14:59:04 -0500

    added ians changes

---
+++ Selection_Sort.cpp
@@ -0,0 +1,17 @@
+#include "myheaders.h"
+#include <iostream>
+using namespace std;
+
+void selection_sort(long data[], int n){
+	for(int i = 0; i < n - 1; i++){
+		int least = i;
+		for(int j = i + 1; j < n; j++){
+			if(data[j] < data[least]){
+				least = j;
+			}
+		}
+		swap(data[i], data[least]);
+	}
+}
+
+
--- myheaders.h
+++ myheaders.h
@@ -19,5 +19,6 @@ void swap(long& x, long& y);
 
 //sorting algorithms
 void bubble_sort(long data[], int n);
+void selection_sort(long data[], int n);
 
 #endif /* MYHEADERS_H_ */

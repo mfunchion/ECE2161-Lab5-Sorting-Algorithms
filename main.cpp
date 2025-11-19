efd7cbc7e6a02cdb0021e460328a8f6db9cec26e
mfunchion <mfunch02@villanova.edu>
2025-11-19 17:06:39 -0500

    sz value

--- main.cpp
+++ main.cpp
@@ -28,7 +28,6 @@ int main() {
 					ifilename = "lab5_input.txt";
 					sz = readfile(ifilename, data);
 					
-					sz = sz/2;
 					
 					if (sz < 0)	 {
 						 cerr << endl << "ERROR: Input File Not Found or File cannot be opened." <<endl;
